81f3111aee12c39302ba8ba1433592b20ebac5da
dannyhuang <kuanhsuh@gmail.com>
2019-01-19 21:50:24 +0800

    45 Animating Hamburger Menu Icon Into An X

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -15,7 +15,10 @@ class MobileMenu {
   toggleTheMenu() {
     this.menuContent.toggleClass("site-header__menu-content--is-visible")
     this.siteHeader.toggleClass("site-header--is-expanded")
+    this.menuIcon.toggleClass("site-header__menu-icon--close-x")
   }
+
+
 
 }
 
