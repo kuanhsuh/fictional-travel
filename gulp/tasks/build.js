f051b7aa9d060896aa46a44349235cfb285de1da
dannyhuang <kuanhsuh@gmail.com>
2019-01-25 23:01:05 +0800

    59 Preparing our files for Go Live

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,13 +1,26 @@
 var gulp = require('gulp'),
 imagemin = require('gulp-imagemin'),
 usemin = require('gulp-usemin'),
-del = require('del')
+del = require('del'),
+rev = require('gulp-rev'),
+cssnano = require('gulp-cssnano'),
+uglify = require('gulp-uglify'),
+browserSync = require('browser-sync').create()
+
+gulp.task('previewDist', function () {
+  browserSync.init({
+    // notify: false,
+    server: {
+      baseDir: "dist"
+    }
+  })
+})
 
 gulp.task('deleteDistFolder', function () {
   return del("./dist")
 })
 
-gulp.task('optimizeImages', ['deleteDistFolder'], function() {
+gulp.task('optimizeImages', ['deleteDistFolder', 'icons'], function() {
   return gulp.src(['./app/assets/images/**/*', '!./app/assets/images/icons', '!./app/assets/images/icons/**/*'])
   .pipe(imagemin({
     progressive: true,
@@ -17,9 +30,12 @@ gulp.task('optimizeImages', ['deleteDistFolder'], function() {
   .pipe(gulp.dest("./dist/assets/images"))
 });
 
-gulp.task('usemin', ['deleteDistFolder'], function() {
+gulp.task('usemin', ['deleteDistFolder', 'styles', 'scripts'], function() {
   return gulp.src("./app/index.html")
-    .pipe(usemin())
+    .pipe(usemin({
+      css: [function() {return rev()}, function() {return cssnano()}],
+      js: [function () {return rev()}, function() {return uglify()}]
+    }))
     .pipe(gulp.dest("./dist"))
 })
 
